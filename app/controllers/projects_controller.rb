ff26640ea7f48204fd1d2cb9dd1e387e6f108e98
Dyego Costa <dyego.costa@live.com>
2013-09-17 01:37:41 -0300

    Project scaffold with validations

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,30 @@
+class ProjectsController < InheritedResources::Base 
+  before_action :set_project, except: [:index, :new, :create]
+  before_action :authenticate_organization!, except: [:index, :show]
+  before_action :authenticate_ownership!, only: [:edit, :update, :destroy]
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(permitted_params)
+    @project.organization = current_organization
+    create!
+  end
+  
+  private
+    def permitted_params
+      params.require(:project).permit(:title, :description, :repository_url, :status)
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+    def authenticate_ownership!
+      unless @project.organization == current_organization
+        redirect_to projects_path 
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AltruistDeveloper::Application.routes.draw do
+  resources :projects
+
   devise_for :organizations
   devise_for :developers
   root "home#index"
