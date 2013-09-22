5c9fc6d3c769a622b0951ea8a1b8e359a2d76bcf
Dyego Costa <dyego.costa@live.com>
2013-09-22 13:52:01 -0300

    Changed the developer sign in method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def signed_in_user_email
+    if developer_signed_in?
+      current_developer.email
+    elsif organization_signed_in?
+      current_organization.email
+    end
+  end
 end
