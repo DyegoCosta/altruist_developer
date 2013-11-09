8ff4ca96c933731d8481ad5f1a7c93b1cba8aa0f
Dyego Costa <dyego.costa@live.com>
2013-11-09 17:44:18 -0200

    Stylizing layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
   def index
-    if signed_in?
-      return redirect_to projects_path
-    else
-      render layout: 'landing_page'
-    end
+    redirect_to projects_path if signed_in?
+  end
+
+  def about
   end
 end
