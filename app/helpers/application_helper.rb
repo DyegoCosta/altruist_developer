949dd9ae85c968806a5e9bffaf7a9ae003c5f9ea
Dyego Costa <dyego.costa@live.com>
2013-11-12 00:52:13 -0200

    Show the name of the signed in user at the menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       current_organization.email
     end
   end
+
+  def signed_in_user_name
+    if developer_signed_in?
+      current_developer.name
+    elsif organization_signed_in?
+      current_organization.name
+    end
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,24 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
+  describe '#signed_in_user_name' do
+    it 'returns organization name' do
+      @request.env['devise.mapping'] = Devise.mappings[:organization]
+      organization = create :organization
+      sign_in organization
+
+      helper.signed_in_user_name.should == organization.name
+    end
+
+    it 'returns developer name' do
+      @request.env['devise.mapping'] = Devise.mappings[:developer]
+      developer = create :developer
+      sign_in developer
+
+      helper.signed_in_user_name.should == developer.name
+    end
+  end
+
   describe '#signed_in_user_email' do
     it 'returns organization email' do
       @request.env['devise.mapping'] = Devise.mappings[:organization]
